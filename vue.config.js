00570d7698742127bf8d3098e8b7bc425dadb764
evelllyn <tinghsin027@gmail.com>
2025-01-13 18:37:09 +0800

    v1.0

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/webpractice/dist'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/webpractice/dist'
+    : '/'
 })
